544a95382b1c3c94e8bffb8cc6dce097518487ec
karnzx <karnzx-18762@hotmail.com>
2021-07-05 19:11:19 +0700

    modified

--- bin/digital_clock.dart
+++ bin/digital_clock.dart
@@ -1,3 +1,42 @@
+// on 18 0
+// set
+// inc
+// set
+// inc
+// set
+// inc
+// inc
+// set
+class Clock {
+  final List<State> states = [Idle(), SettingHours(), SettingMins()];
+  final int currentState = 0;
+}
+
+abstract class State {
+  void turnOn(Clock clock) {
+    print('error');
+  }
+}
+
+class Idle extends State {}
+
+class SettingHours extends State {}
+
+class SettingMins extends State {}
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  List<String> msgs = [
+    'on 18 0',
+    'set',
+    'inc',
+    'set',
+    'inc',
+    'set',
+    'inc',
+    'inc',
+    'set'
+  ];
+  for (var msg in msgs) {
+    print(msgs);
+  }
 }
